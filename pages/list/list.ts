d90433c30fc46849ebe72a35b8537e7c1fedd06e
jordi zaplana <jordi.zaplana.gil@gmail.com>
2018-04-24 17:54:58 +0200

    master-init

---
+++ pages/list/list.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-list',
+  templateUrl: 'list.html'
+})
+export class ListPage {
+  constructor(public navCtrl: NavController) {
+  }
+//  goToHistRicoSensores(params){
+//    if (!params) params = {};
+//  this.navCtrl.push(HistRicoSensoresPage);
+//  }
+}
+
+
+
+
---
+++ pages/list_1/list_1.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-list',
+  templateUrl: 'list_1.html'
+})
+export class ListPage_1 {
+  constructor(public navCtrl: NavController) {
+  }
+//  goToHistRicoSensores(params){
+//    if (!params) params = {};
+//  this.navCtrl.push(HistRicoSensoresPage);
+//  }
+}
+
+
+
+
