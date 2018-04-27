8a9c5f0b57aceef722607ff694b2f95cef84d35a
JordiZaplana <34708628+JordiZaplana@users.noreply.github.com>
2018-04-27 07:32:04 +0200

    pantalla ajustes

--- pages/toolsConfig/toolsConfig.ts
+++ pages/toolsConfig/toolsConfig.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HomePage } from '../home/home';
+
 
 @Component({
   selector: 'page-list',
@@ -8,6 +10,16 @@ import { NavController } from 'ionic-angular';
 export class toolsConfig_1 {
   constructor(public navCtrl: NavController) {
   }
+  public min_distancia:string; 
+  pushPage_cancelar() {
+    this.navCtrl.push(HomePage);
+  };
+
+  pushPage_confirmar() {
+    // set->firebase
+    //this.navCtrl.push(HomePage);
+    return this.min_distancia;
+  };
 
 }
 
